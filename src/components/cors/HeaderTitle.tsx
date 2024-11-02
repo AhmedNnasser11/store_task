23edd5813d079946583d204727d5799f4c5358be
ahmednnasser11 <Ahmednnasser11@gmail.com>
2024-11-02 13:47:54 +0200

    fix color

--- src/components/cors/HeaderTitle.tsx
+++ src/components/cors/HeaderTitle.tsx
@@ -1,4 +1,4 @@
 const HeaderTitle = ({ children }: { children: React.ReactNode }) => {
-  return <h1 className="text-2xl font-semibold mb-6">{children}</h1>;
+  return <h1 className="text-2xl font-semibold mb-6 text-black">{children}</h1>;
 };
 export default HeaderTitle;
